896c59a58610d22f6d111268bc60b6cb362cc4e1
xindi-wang <wangcd94@gmail.com>
2018-12-04 13:38:45 -0500

    fixed empty categories

--- js/draw.js
+++ js/draw.js
@@ -107,8 +107,10 @@ function getTopTalks(category, data) {
   data.forEach(function(d, i) {
     var dict = [];
 
+    var category_text = d['tags'].split(',')[0].toLowerCase();
 
-    if (d['tags'].split(',')[0] == category && counter <= 4) //if there is a match, display link & tite of talk
+
+    if (category_text == category && counter <= 4) //if there is a match, display link & tite of talk
     {
       item_array = [];
       var list = "";
