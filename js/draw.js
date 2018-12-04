f827095c9dbdd41b7a0fc7033228bb6e3858e932
pansyjia <siyujia@umich.edu>
2018-12-04 11:39:44 -0500

    color added

--- js/draw.js
+++ js/draw.js
@@ -5,6 +5,26 @@
   //var height = 600; //declares height of container variable and assigns it value of 100
   //var rectWidth = 15;       //sets rectWidth variable to be 20
 
+
+  var category_colors = {
+    // implement this based on what we did in class
+    "": { desc: "Married", color: "#5B7BE9" },
+    "Beautiful": { desc: "Own children in Household", color: "#E0D22E" },
+    "Confusing": { desc: "Has Healthcare Coverage", color: "#2CCEF6" },
+    "Courageous": { desc: "Bachelor's Degree or More", color: "#FB7F23" },
+    // "employed": { desc: "Employed", color: "#D63CA3" },
+    // "selfemp": { desc: "Self-employed<sup>*</sup>", color: "#c38014" },
+    // "publictrans": { desc: " Primarily Pub. Trans. to Work<sup>*</sup>", color: "#E24062" },
+    // "income_moremed": { desc: "Personal Income Above Nat. Med.", color: "#5BB923" },
+    // "inpoverty": { desc: "Below Poverty Line", color: "#555" },
+    // "isveteran": { desc: "Veteran", color: "#B190D0" },
+    // "bornoutus": { desc: "Born Outside US", color: "#bcc832" },
+    // "diffmovecog": { desc: "Cog. or Phys. Difficulty", color: "#ee7b9c" },
+    // "diffhearvis": { desc: "Hearing or Vis. Difficulty", color: "#f299b3"},
+    // "widowed": { desc: "Widowed", color: "#01d99f" },
+}
+
+
   var data = [];
   var val
   var talk_categories = ["art"];
@@ -71,13 +91,19 @@
   {
     console.log(category);
     //set up margin and scale
-    var margin = { top: 20, right: 20, bottom: 30, left: 60 },
+    ////////////////////////////////////////////////////////////////////////////////////////
+    ///////////////Start///////////////////
+    var margin = { top: 20, right: 20, bottom: 80, left: 60 },
         width = 500 - margin.left - margin.right,
-        height = 500 - margin.top - margin.bottom;
+        height = 350 - margin.top - margin.bottom;
+    ///need to change size to make the chart look better and show x labels
+    ////////////////////////////////////////////////////////////////////////////////////////
+    ///////////////End///////////////////
 
     var x = d3.scaleBand().range([0, width]).padding(0.1);
 
     var y = d3.scaleLinear().range([height, 0]);
+
 
     var counter = 0;
     var item_array;
@@ -117,6 +143,8 @@
                 talk_chart = d3.select('#list').append("div").attr("class","talk_chart")
                 item_array.push(list);
                 talk_info.html(list);
+
+              
                 var svg = talk_chart.append("svg")
                     .attr("width", width + margin.left + margin.right)
                     .attr("height", height + margin.top + margin.bottom)
@@ -126,6 +154,12 @@
               x.domain(dict.map(function (d) { return d.key; }));
               y.domain([0, d3.max(dict, function (d) { return d.value; })]);
 
+
+     ////////////////////////////////////////////////////////////////////////////////////////
+    ///////////////Start///////////////////
+              var tooltip = d3.select(".talk_chart").append("div").attr("class", "toolTip");
+ ////////////////////////////////////////////////////////////////////////////////////////
+    ///////////////End///////////////////
 
                 svg.selectAll(".bar")
                 .data(dict)
@@ -137,6 +171,36 @@
                 .attr("y", function (d) { return y(d.value); })
                 .attr("height", function (d) { return height - y(d.value); })
 
+     ////////////////////////////////////////////////////////////////////////////////////////
+    ///////////////Start///////////////////
+                .attr("opacity", "0.7")
+                .on("mousemove", function (d) {
+                        d3.select(this).attr("opacity", "1");
+                        tooltip.style("left", d3.event.pageX - 50 + "px")
+                            .style("top", d3.event.pageY - 50 + "px")
+                            .style("display", "inline-block")
+                            .html("<b>" +(d.key) + "</b> : " + (d.value));
+                })
+                .on("mouseout", function (d) {
+                        d3.select(this).attr("opacity", "0.7");
+                        tooltip.style("display", "none");
+                });
+
+            // add the x Axis
+            svg.append("g")
+                .attr("transform", "translate(0," + height + ")")
+                .call(d3.axisBottom(x))
+                .selectAll("text")
+                .style("text-anchor", "end")
+                .attr("dx", "-.8em")
+                .attr("dy", ".15em")
+                .attr("transform", "rotate(-65)");
+
+            // add the y Axis
+            svg.append("g")
+                .call(d3.axisLeft(y));
+////////////////////////////////////////////////////////////////////////////////////////
+    ///////////////End///////////////////
 
                 counter += 1;
                 }
