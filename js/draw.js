b685998da17c3b2cb266b7ac7e6bcd1aa5a74a49
xindi-wang <wangcd94@gmail.com>
2018-12-03 21:51:34 -0500

    5 items working

--- js/draw.js
+++ js/draw.js
@@ -1,34 +1,36 @@
-document.addEventListener('DOMContentLoaded', function () {
+//document.addEventListener('DOMContentLoaded', function () {
 
 
   //var width = 900;  //declares width of container variable and assigns it a value of 960
   //var height = 600; //declares height of container variable and assigns it value of 100
   //var rectWidth = 15;       //sets rectWidth variable to be 20
-  
+
   var data = [];
+  var val
   var talk_categories = ["art"];
   var list = "";
   var selectValue = "3d printing";
-  
+
   $(document).ready(function () {
       console.log("yo");
       loadData();
   });
-  
-  // Loads the CSV file 
+
+  // Loads the CSV file
   function loadData() {
-       console.log("HEY"); 
-      // load the csv file    
+       console.log("HEY");
+      // load the csv file
       // assign it to the data variable
-      d3.csv("data/ted_clean.csv", function (d) {       
-              data = d;  
+      d3.csv("data/ted_clean.csv", function (d) {
+              data = d;
+              val = data;
               data.sort(function(x, y){ //sort the talks by views
                 return d3.ascending(y.views);
-             }) 
+             })
               data.forEach(function(d,i)  //loop through data rows
               {
                 var category = d['tags'].split(',')[0].toLowerCase(); //get category
-                if (talk_categories.includes(category) == false) 
+                if (talk_categories.includes(category) == false)
                 {
                   talk_categories.push(category);
                   //add category to "talk categories" array if not already there
@@ -45,26 +47,25 @@ document.addEventListener('DOMContentLoaded', function () {
               .html(getTopTalks(selectValue, data));
               //console.log(talk_categories[0]);
              // console.log(getTopTalks('art',data));
-              
+
           });
-  
-  
+
+
   }
-  
-  
-  
+
+
+
   function getTopTalks(category, data)
   {
     console.log(category);
     list = "";
     var counter = 0;
-     data.forEach(function(d,i)
+    var item_array = [];
+    data.forEach(function(d,i)
               {
-         
-  
                 //FORMATTING NEEDED HERE!!!!///
-  
-                if (d['tags'].split(',')[0] == category) //if there is a match, display link & tite of talk
+
+                if (d['tags'].split(',')[0] == category && counter <= 4) //if there is a match, display link & tite of talk
                 {
                   list += '<br>';
                   list += ("Title: " + (d['title']));
@@ -75,15 +76,20 @@ document.addEventListener('DOMContentLoaded', function () {
                 list += '<br>';
                   list += (" Link: " + d['url']);
                 list += '<br>';
+
+                item_array.push(list);
+                counter += 1;
                 }
+
               });
-  
+              console.log(counter)
+
               return list;
-            
+
   }
 
 
-  
+
   function setDropdownOptions(data)
   {
     //talk_categories.toLowerCase(); //make it all lower case
@@ -93,18 +99,18 @@ document.addEventListener('DOMContentLoaded', function () {
     talk_categories.shift();
     talk_categories.shift();
     console.log(talk_categories);
-  
-  var select = d3.select('#container') 
+
+  var select = d3.select('#container')
     .append('select')
       .attr('class','select')
       .on('change',onchange)
-  
+
   var options = select
     .selectAll('option')
     .data(talk_categories).enter()
     .append('option')
       .text(function (d, i) { return (talk_categories[i]); });
-  
+
   d3.select('.select') //update list on change of category
     .on('change', function() {
       selectValue = d3.select('select').property('value');
@@ -114,10 +120,10 @@ document.addEventListener('DOMContentLoaded', function () {
       .append('p')
       // .text(function() { console.log(getTopTalks(selectValue, data)); return getTopTalks(selectValue, data); });
       .html(getTopTalks(selectValue, data));
+      d3.select('#list').append('div');
   });
-  
+
   }
-  
-  
-  });
-  
+
+
+  //});
